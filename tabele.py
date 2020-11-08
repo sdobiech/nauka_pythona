0077597302af2121404967f72061043f1c6d7f35
sdobiech <sdobiech@users.noreply.githyb.com>
2020-11-08 13:14:02 +0100

    python tabele

---
+++ tabele.py
@@ -0,0 +1,6 @@
+rosliny=['drzewka', 'aloesy', 'storczyki', 'inne']
+ilosc=[2, 1, 2, 4]
+
+for idx in range(len(rosliny)):
+    print('idx: '+str(idx)+": "+rosliny[idx])
+    print(rosliny[idx]+ " mam ich " +str(ilosc[idx]))
