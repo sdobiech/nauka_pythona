cc143dc220fd6e5363af70f0e54c9ba5954e52b1
sdobiech <sdobiech@user.noreply.github.com>
2020-11-14 14:45:05 +0100

    rozwianie zadania12

--- zadanie12.py
+++ zadanie12.py
@@ -1,6 +1,6 @@
-rosliny_items=['storczyk', 'dracena', 'fikus', 'aloes']
-rosliny_ilosc=[1, 2, 1, 3]
-rosliny_ceny=[300, 200, 100, 50]
+rosliny_items=['storczyk', 'dracena']
+rosliny_ilosc=[4, 1]
+rosliny_ceny=[150, 120]
 
 suma=0.0
 for idx in range(len(rosliny_items)):
@@ -10,17 +10,13 @@ for idx in range(len(rosliny_items)):
     print("{0} {1} {2}" .format(item, ile, cena))
     suma=suma+cena
 
-'''if 'storczyk' in rosliny_items and 'dracena' in rosliny_items: #r2
-    print("Kwota przed obnizka: ", suma)
-    print('Znizka')
-    suma=suma-(suma*10)/100'''
-
-if len(rosliny_items) > 3: #r1
+if len(rosliny_items) > 3 and suma <= 500: #r1
     suma=suma-(suma*5)/100
 
-if suma >= 500: #r2
-    print("Znizka R2:")
+elif len(rosliny_items) >3 and suma >= 500: #r2
     suma=suma-(suma*10)/100
 
+elif len(rosliny_items) <3 and suma >= 500: #r2
+    suma=suma-(suma*10)/100
 
 print("Wartosc koszyka: {0}" .format(suma))
